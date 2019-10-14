9f9ce83ad5e7cf245c360cbaa398839815c9d199
ThePoptartTARDIS <44676482+ThePoptartTARDIS@users.noreply.github.com>
2019-10-14 11:34:54 -0700

    rokipilled

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 brick.buttonEnter.onEvent(ButtonEvent.Pressed, function () {
-    brick.setStatusLight(StatusLight.GreenFlash)
-    brick.showString("Hello world", 1)
-    brick.showString("Everything is Awesome", 2)
+    brick.setStatusLight(StatusLight.RedPulse)
+    brick.showString("Da li je to istina ili se samo salis,", 1)
+    brick.showString("Na Balkanu hoces vatru da zapalis", 2)
+    brick.showString("Roki Vulovic.", 3)
 })
-
